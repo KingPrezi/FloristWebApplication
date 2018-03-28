3803d6b2cd1c966858257e8f5402c54e4b7c7ac5
kholofelochoeu@gmail.com <User@S7>
2018-03-28 14:12:05 +0200

    Some changes to supplier

---
+++ target/generated-sources/annotations/com/NetFlorist/model/Product_.java
@@ -0,0 +1,19 @@
+package com.NetFlorist.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-28T13:20:12")
+@StaticMetamodel(Product.class)
+public class Product_ { 
+
+    public static volatile SingularAttribute<Product, String> image;
+    public static volatile SingularAttribute<Product, Integer> quantity;
+    public static volatile SingularAttribute<Product, Integer> productID;
+    public static volatile SingularAttribute<Product, Double> price;
+    public static volatile SingularAttribute<Product, String> name;
+    public static volatile SingularAttribute<Product, Integer> totalq;
+    public static volatile SingularAttribute<Product, String> category;
+
+}
